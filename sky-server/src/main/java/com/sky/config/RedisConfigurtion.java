16d0336c4dcd0c29335a8a1c0ed39b7c93575cf2
本月 肖 <1211564349@qq.com>
2024-07-25 05:57:29 +0800

    店铺营业状态设置功能开发

---
+++ sky-server/src/main/java/com/sky/config/RedisConfigurtion.java
@@ -0,0 +1,25 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@Slf4j
+public class RedisConfigurtion {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建控制类模板对象...");
+        RedisTemplate  redisTemplate = new RedisTemplate();
+        //设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置key value的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
